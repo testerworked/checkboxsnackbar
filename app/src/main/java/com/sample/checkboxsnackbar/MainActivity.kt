cd2800ab380aa5e39bf4edf5e12aa8b0ec14b397
Khuzin Rinat <khuzin.rinat.work@gmail.com>
2024-10-31 10:04:31 +0500

    add: buttons save and delete;

--- app/src/main/java/com/sample/checkboxsnackbar/MainActivity.kt
+++ app/src/main/java/com/sample/checkboxsnackbar/MainActivity.kt
@@ -1,20 +1,62 @@
 package com.sample.checkboxsnackbar
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textET: EditText
+    private lateinit var outputTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        textET = findViewById(R.id.textET)
+        outputTextView = findViewById(R.id.outputTextView)
+
+        val saveButton = findViewById<Button>(R.id.saveButton)
+        saveButton.setOnClickListener {
+            saveData()
+        }
+
+        val deleteButton = findViewById<Button>(R.id.deleteButton)
+        deleteButton.setOnClickListener {
+            showDeleteDialog()
+        }
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    private fun saveData() {
+        val inputText = textET.text.toString()
+        outputTextView.text = outputTextView.text.toString()+ "\n" + inputText
+    }
+
+    private fun showDeleteDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setMessage("Подтвердите удаление")
+            .setPositiveButton("Удалить", DialogInterface.OnClickListener { dialog, id ->
+                outputTextView.text = ""
+                Snackbar.make(findViewById(android.R.id.content), "Данные удалены", Snackbar.LENGTH_SHORT).show()
+            })
+            .setNegativeButton("Отмена", DialogInterface.OnClickListener { dialog, id ->
+                dialog.dismiss()
+            })
+        builder.create().show()
     }
 }
